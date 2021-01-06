0df15cec27f012a20bf235ea2cdc4e4b7e6b0c84
Tanner Winkelman <winkelmantanner@gmail.com>
2021-01-06 17:17:29 -0600

    Improvement on Politician AI.

--- SlanPol.java
+++ SlanPol.java
@@ -12,32 +12,31 @@ public strictfp class SlanPol extends Unit {
     public void runTurnPolitician() throws GameActionException {
         final int conv_available = rc.getConviction() - 10;
         final MapLocation myLoc = rc.getLocation();
-        int actionR2 = rc.getType().actionRadiusSquared;
-        int transferrableConviction = 0;
-        if(conv_available >= 1) {
-            RobotInfo [] rbts = rc.senseNearbyRobots(actionR2);
-            for(RobotInfo rbt : rbts) {
-                System.out.println(String.valueOf(rbt.getID()) + " " + rbt.getLocation().toString() + " " + rbt.getTeam().toString() + " " + rbt.getType().toString() + " " + String.valueOf(rbt.getInfluence()) + " " + String.valueOf(rbt.getConviction()));
-                if(rbt.team != rc.getTeam()) {
-                    transferrableConviction += (conv_available / rbts.length);
+        for(int r_unsquared = 1; r_unsquared * r_unsquared <= rc.getType().actionRadiusSquared; r_unsquared++) {
+            int actionR2 = r_unsquared * r_unsquared;
+            int transferrableConviction = 0;
+            if(conv_available >= 1) {
+                RobotInfo [] rbts = rc.senseNearbyRobots(actionR2);
+                for(RobotInfo rbt : rbts) {
+                    System.out.println(String.valueOf(rbt.getID()) + " " + rbt.getLocation().toString() + " " + rbt.getTeam().toString() + " " + rbt.getType().toString() + " " + String.valueOf(rbt.getInfluence()) + " " + String.valueOf(rbt.getConviction()));
+                    if(rbt.team != rc.getTeam()) {
+                        transferrableConviction += (conv_available / rbts.length);
+                    }
                 }
             }
-        }
-        if (rc.canEmpower(actionR2) &&
-            transferrableConviction
-            > conv_available
-                * recipDecay(
-                    conv_available,
-                    100
-                )
-        ) {
-            System.out.println("empowering  my conviction:" + String.valueOf(rc.getConviction()) + " actionR2:" + String.valueOf(actionR2));
-            rc.empower(actionR2);
+            if (rc.canEmpower(actionR2) &&
+                transferrableConviction
+                > conv_available
+                    * recipDecay(
+                        conv_available,
+                        100
+                    )
+            ) {
+                System.out.println("empowering  my conviction:" + String.valueOf(rc.getConviction()) + " actionR2:" + String.valueOf(actionR2));
+                rc.empower(actionR2);
+            }
         }
         if (tryMove(randomDirection())) {}
-        // if(rc.getRoundNum() == 1950) {
-        //     System.out.println("my conviction:" + String.valueOf(rc.getConviction()));
-        // }
     }
     public void runTurnSlanderer() throws GameActionException {
         if (tryMove(randomDirection())) {}
