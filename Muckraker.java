074d245d4caeb03380bd77604821262276f59b5b
Tanner Winkelman <21373626+winkelmantanner@users.noreply.github.com>
2021-01-07 13:46:54 -0600

    Make muckrakers move toward emeny slanderers

--- Muckraker.java
+++ Muckraker.java
@@ -3,16 +3,33 @@ import battlecode.common.*;
 
 public strictfp class Muckraker extends Unit {
     RobotController rc;
+    MapLocation where_i_saw_enemy_slanderer = null;
+    int round_when_i_saw_enemy_slanderer = -1;
     Muckraker(RobotController rbt_controller) {
         super(rbt_controller);
         rc = rbt_controller;
     }
     public void runTurn() throws GameActionException {
         Team enemy = rc.getTeam().opponent();
-        int actionRadius = rc.getType().actionRadiusSquared;
-        for (RobotInfo robot : rc.senseNearbyRobots(actionRadius, enemy)) {
+        int sensorRadius = rc.getType().sensorRadiusSquared;
+        MapLocation myLoc = rc.getLocation();
+
+        /*
+        Note, within the sensor radius, I can detect all attributes of robots.
+        Within the detection radius, I can only detect their location.
+        So use the sensor radius.
+        */
+        for (RobotInfo robot : rc.senseNearbyRobots(sensorRadius, enemy)) {
             if (robot.type.canBeExposed()) {
                 // It's a slanderer... go get them!
+                if(
+                    round_when_i_saw_enemy_slanderer != rc.getRoundNum()
+                    || where_i_saw_enemy_slanderer == null
+                    || myLoc.distanceSquaredTo(robot.location) < myLoc.distanceSquaredTo(where_i_saw_enemy_slanderer)
+                ) {
+                    where_i_saw_enemy_slanderer = robot.location;
+                    round_when_i_saw_enemy_slanderer = rc.getRoundNum();
+                }
                 if (rc.canExpose(robot.location)) {
                     System.out.println("e x p o s e d");
                     rc.expose(robot.location);
@@ -20,6 +37,16 @@ public strictfp class Muckraker extends Unit {
                 }
             }
         }
-        if (tryMove(randomDirection())) {}
+
+        // Try to move toward enemy slanderers seen recently
+        if (
+            where_i_saw_enemy_slanderer != null
+            && 30 > rc.getRoundNum() - round_when_i_saw_enemy_slanderer
+            && fuzzyStep(where_i_saw_enemy_slanderer)
+        ) {
+            System.out.println("I (muckraker) stepped toward target: " + where_i_saw_enemy_slanderer.toString());
+        }
+
+        tryMove(randomDirection());
     }
 }
