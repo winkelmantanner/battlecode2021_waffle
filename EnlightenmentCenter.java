26f2dedc06686466fe29c6f1b456bbfc883b27ae
Tanner Winkelman <winkelmantanner@gmail.com>
2021-01-08 09:38:15 -0600

    Make EC bidding more interesting

--- EnlightenmentCenter.java
+++ EnlightenmentCenter.java
@@ -4,6 +4,14 @@ import battlecode.common.*;
 public strictfp class EnlightenmentCenter extends Robot {
     RobotController rc;
     int numRobotsBuilt = 0;
+
+    // This does not get initialized.
+    // Use it to determine if other friendly ECs are bidding.
+    int last_round_team_votes = 0;
+
+    boolean i_should_bid = true;
+    boolean i_bidded_last_round = false;
+
     EnlightenmentCenter(RobotController rbt_controller) {
         super(rbt_controller);
         rc = rbt_controller;
@@ -32,8 +40,30 @@ public strictfp class EnlightenmentCenter extends Robot {
                 numRobotsBuilt++;
             }
         }
-        if(rc.canBid(1)) {
-            rc.bid(1);
+
+        if(
+            rc.getTeamVotes() > last_round_team_votes
+            && !i_bidded_last_round
+        ) {
+            if(i_should_bid) {
+                System.out.println("changing i_should_bid to false");
+            }
+            // Another friendly EC is bidding
+            i_should_bid = false;
         }
+        i_bidded_last_round = false;
+        if(i_should_bid) {
+            int amount = (int) (Math.random() * rc.getInfluence() / 20);
+            if(rc.canBid(amount)) {
+                rc.bid(amount);
+                i_bidded_last_round = true;
+                System.out.println("I bidded " + String.valueOf(amount));
+            } else {
+                System.out.println("I could not bid " + String.valueOf(amount));
+            }
+            // When amount is zero, the EC will not bid.
+            // That is how it sees if other friendly ECs are bidding.
+        }
+        last_round_team_votes = rc.getTeamVotes();
     }
 }
