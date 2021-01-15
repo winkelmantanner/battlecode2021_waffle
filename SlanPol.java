44308b093e224e03c5501607db4c2ffc20116c54
Tanner Winkelman <winkelmantanner@gmail.com>
2021-01-14 19:01:20 -0600

    Add defender politicians

--- SlanPol.java
+++ SlanPol.java
@@ -6,9 +6,13 @@ public strictfp class SlanPol extends Unit {
 
     int id_of_ec_to_look_to = -1;
 
+    boolean is_defender = false;
+
     SlanPol(RobotController rbt_controller) {
         super(rbt_controller);
         last_round_type = rc.getType();
+        is_defender = rc.getType().equals(RobotType.POLITICIAN)
+            && (rc.getInfluence() <= MAX_DEFENDER_INFLUENCE);
 
         for(RobotInfo rbt : rc.senseNearbyRobots(
             RobotType.ENLIGHTENMENT_CENTER.actionRadiusSquared,
@@ -20,16 +24,21 @@ public strictfp class SlanPol extends Unit {
         }
     }
 
-    void empowerIfApplicable() throws GameActionException {
-        final double conv_available = (
+    double getEmpowerConvAvailable() throws GameActionException {
+        return (
             rc.getConviction() * rc.getEmpowerFactor(rc.getTeam(), 0)
         ) - 10;
+    }
+    void empowerIfApplicable() throws GameActionException {
+        final double conv_available = getEmpowerConvAvailable();
         for(int r_unsquared = 1; r_unsquared * r_unsquared <= rc.getType().actionRadiusSquared; r_unsquared++) {
             int actionR2 = r_unsquared * r_unsquared;
             double transferrableConviction = 0;
             boolean just_do_it = false;
+            int rbts_len = -1; // This variable is used for logs only
             if(conv_available >= 1) {
                 RobotInfo [] rbts = rc.senseNearbyRobots(actionR2);
+                rbts_len = rbts.length;
                 for(RobotInfo rbt : rbts) {
                     if(rbt.team != rc.getTeam()) {
                         if(
@@ -40,7 +49,7 @@ public strictfp class SlanPol extends Unit {
                             just_do_it = true;
                         }
                         if(rbt.type.equals(RobotType.MUCKRAKER)) {
-                            transferrableConviction += Math.min(rbt.conviction + 1, conv_available / rbts.length);
+                            transferrableConviction += Math.min(rbt.conviction + 1, Math.floor(conv_available / rbts.length));
                         } else if(rbt.type.equals(RobotType.POLITICIAN)) {
                             transferrableConviction += Math.min(rbt.conviction + rbt.influence, conv_available / rbts.length);
                         } else {
@@ -61,26 +70,52 @@ public strictfp class SlanPol extends Unit {
                     )
                 )
             ) {
-                System.out.println("empowering  my conviction:" + String.valueOf(rc.getConviction()) + " actionR2:" + String.valueOf(actionR2) + " transferrableConviction:" + String.valueOf(transferrableConviction));
+                System.out.println("empowering  my conviction:" + String.valueOf(rc.getConviction()) + " actionR2:" + String.valueOf(actionR2) + " transferrableConviction:" + String.valueOf(transferrableConviction) + " conv_available:" + String.valueOf(conv_available) + " rbts_len:" + String.valueOf(rbts_len));
                 rc.empower(actionR2);
             }
         }
     }
 
-    void moveTowardSensableRobotsIfApplicable() throws GameActionException {
+    final int THREATENING_MR_FOLLOW_DIST2 = 11*11;
+    final int THREATENING_MR_EMPOWER_DIST2 = 9*9;
+    void empowerOnThreateningMuckrakers() throws GameActionException {
+        RobotInfo threatening_mr = nearestRobot(
+            null,
+            rc.getType().actionRadiusSquared,
+            rc.getTeam().opponent(),
+            RobotType.MUCKRAKER
+        );
+        if(threatening_mr != null
+            && where_i_spawned.distanceSquaredTo(threatening_mr.location) < THREATENING_MR_EMPOWER_DIST2
+        ) {
+            // Do not empower unless destruction of the muckraker is guaranteed.
+            // Try the dist2 to the muckraker and try the maximum dist2.
+            int dist2_to_mr = rc.getLocation().distanceSquaredTo(threatening_mr.location);
+            final int num_rbts_minimum = rc.senseNearbyRobots(dist2_to_mr).length;
+            final int num_rbts_maximum = rc.senseNearbyRobots(rc.getType().actionRadiusSquared).length;
+            final double conv_available = getEmpowerConvAvailable();
+            int dist2_to_empower = -1;
+            if(conv_available / num_rbts_maximum >= 1 + threatening_mr.conviction) {
+                dist2_to_empower = rc.getType().actionRadiusSquared;
+            } else if(conv_available / num_rbts_minimum >= 1 + threatening_mr.conviction) {
+                dist2_to_empower = dist2_to_mr;
+            } else {
+                System.out.println("Did not empower solely because I could not guarantee the destruction of the muckraker at " + threatening_mr.location.toString());
+            }
+            if(dist2_to_empower > 0
+                && rc.canEmpower(dist2_to_empower)
+            ) {
+                System.out.println("empowering on muckraker  myconviction:" + String.valueOf(rc.getConviction()) + " dist2_to_empower:" + String.valueOf(dist2_to_empower) + " conv_available:" + String.valueOf(conv_available));
+                rc.empower(dist2_to_empower);
+            }
+        }
+    }
+
+    void moveTowardNonfriendlyEcs() throws GameActionException {
         if(rc.isReady()) {
-            int dist2_to_nearest_target = 12345;
-            RobotInfo target_rbt = null;
-            RobotInfo [] rbts = rc.senseNearbyRobots(rc.getType().sensorRadiusSquared);
-            for(RobotInfo rbt : rbts) {
-                if(
-                    rbt.type.equals(RobotType.ENLIGHTENMENT_CENTER)
-                    && !rbt.team.equals(rc.getTeam())
-                    && rc.getLocation().distanceSquaredTo(rbt.location) < dist2_to_nearest_target
-                ) {
-                    target_rbt = rbt;
-                    dist2_to_nearest_target = rc.getLocation().distanceSquaredTo(rbt.location);
-                }
+            RobotInfo target_rbt = nearestRobot(null, -1, Team.NEUTRAL, RobotType.ENLIGHTENMENT_CENTER);
+            if(target_rbt == null) {
+                target_rbt = nearestRobot(null, -1, rc.getTeam().opponent(), RobotType.ENLIGHTENMENT_CENTER);
             }
             if(target_rbt != null) {
                 fuzzyStep(target_rbt.location);
@@ -88,6 +123,36 @@ public strictfp class SlanPol extends Unit {
         }
     }
 
+    void moveTowardEnemyMuckrakers() throws GameActionException {
+        if(rc.isReady()) {
+            RobotInfo target_rbt = nearestRobot(null, -1, rc.getTeam().opponent(), RobotType.MUCKRAKER);
+            if(target_rbt != null
+                && where_i_spawned.distanceSquaredTo(target_rbt.location) < THREATENING_MR_FOLLOW_DIST2
+            ) {
+                fuzzyStep(target_rbt.location);
+            }
+        }
+    }
+
+    final int NEAR_HOME_DIST2 = 7*7;
+    boolean spreadNearHome() throws GameActionException {
+        boolean moved = false;
+        RobotInfo nearby_robot = nearestRobot(null, 2, null, null);
+        if(nearby_robot != null) {
+            if(fuzzyStep(rc.getLocation().directionTo(nearby_robot.location).opposite())) {
+                moved = true;
+                System.out.println("I stepped away from robot at " + nearby_robot.location.toString());
+            }
+        } else if(rc.getLocation().distanceSquaredTo(where_i_spawned) >= NEAR_HOME_DIST2) {
+            moved = fuzzyStep(where_i_spawned);
+        } else {
+            moved = tryMove(randomDirection());
+        }
+        return moved;
+    }
+
+    MapLocation target_loc_from_flag = null;
+    int round_num_of_flag_read = -1;
     void polFlagReceivingStuff() throws GameActionException {
         if(target_loc_from_flag == null) {
             if(rc.canGetFlag(id_of_ec_to_look_to)) {
@@ -133,21 +198,28 @@ public strictfp class SlanPol extends Unit {
         }
     }
 
-    MapLocation target_loc_from_flag = null;
-    int round_num_of_flag_read = -1;
-
     public void runTurnPolitician() throws GameActionException {
         flagEnemies();
         flagNeutralECs();
         // The second flag overrides the first
 
-        empowerIfApplicable();
+        if(is_defender) { // influence is not reduced by damage
+            // I'm a defender
+            empowerOnThreateningMuckrakers();
 
-        moveTowardSensableRobotsIfApplicable();
+            moveTowardEnemyMuckrakers();
 
-        polFlagReceivingStuff();
+            spreadNearHome();
+        } else {
+            // I'm an attacker
+            empowerIfApplicable();
 
-        exploreMove();
+            moveTowardNonfriendlyEcs();
+
+            polFlagReceivingStuff();
+
+            exploreMove();
+        }
     }
 
     public void runTurnSlanderer() throws GameActionException {
