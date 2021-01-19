a59550135312706c6c10f2c534b7b395f9a6a498
Tanner Winkelman <winkelmantanner@gmail.com>
2021-01-19 16:20:04 -0600

    Crunch slans by flag instead of symmetric maplocations

--- Muckraker.java
+++ Muckraker.java
@@ -4,6 +4,10 @@ import battlecode.common.*;
 public strictfp class Muckraker extends Unit {
     MapLocation where_i_saw_enemy_slanderer = null;
     int round_when_i_saw_enemy_slanderer = -1;
+
+    MapLocation where_flag_indicated_enemy_slanderer = null;
+    int round_when_flag_indicated_enemy_slanderer = -1;
+
     boolean is_cruncher = false;
     Muckraker(RobotController rbt_controller) {
         super(rbt_controller);
@@ -11,73 +15,47 @@ public strictfp class Muckraker extends Unit {
         System.out.println("is_cruncher:" + String.valueOf(is_cruncher));
     }
 
-    boolean knowMapDims() {
-        return map_max_x != UNKNOWN
-            && map_min_x != UNKNOWN
-            && map_max_y != UNKNOWN
-            && map_min_y != UNKNOWN;
-    }
-    MapLocation [] getPlacesEnemyEcMightBe() throws GameActionException {
-        // precondtion: knowMapDims() == true
-        boolean vertical_first = (rc.getID() % 2 == 1);
-        MapLocation [] result = new MapLocation[3];
-        // In the array, order them horizontal, rotational, vertical (unless vertical_first==true).
-        // They are generated in a different order here for calculation convenience.
-        result[0] = new MapLocation(-loc_of_ec_to_look_to.x + map_max_x + map_min_x, loc_of_ec_to_look_to.y);
-        result[2] = new MapLocation( loc_of_ec_to_look_to.x, -loc_of_ec_to_look_to.y + map_max_y + map_min_y);
-        result[1] = new MapLocation(result[0].x, result[2].y);
-        if(vertical_first) {
-            MapLocation temp = result[0];
-            result[0] = result[2];
-            result[2] = temp;
-        }
-        return result;
-    }
-    int index_in_places = 0;
-    boolean is_adj_to_enemy_ec = false;
-    boolean moveForSymmetricEnemyEc() throws GameActionException {
+    boolean moveForSlandererFromFlag() throws GameActionException {
         boolean did_move = false;
         if(
-            knowMapDims()
-            && index_in_places < 3
+            id_of_ec_to_look_to != -1
+            && rc.canGetFlag(id_of_ec_to_look_to)
         ) {
-            MapLocation [] places = getPlacesEnemyEcMightBe();
-            if(rc.canSenseLocation(places[index_in_places])) {
-                RobotInfo rbt = rc.senseRobotAtLocation(places[index_in_places]);
-                if(rbt != null
-                    && rbt.team.equals(rc.getTeam().opponent())
-                    && rbt.type.equals(RobotType.ENLIGHTENMENT_CENTER)
-                ) {
-                    //FOUND IT
-                    if(rc.getLocation().isAdjacentTo(rbt.location)) {
-                        is_adj_to_enemy_ec = true;
-                        System.out.println("is_adj_to_enemy_ec:" + String.valueOf(is_adj_to_enemy_ec) + "; I'm done moving");
-                        // Don't move.
-                    }
-                } else {
-                    // We sense that the enemy EC is not there
-                    index_in_places++;
-                }
+            int flag_val = rc.getFlag(id_of_ec_to_look_to);
+            if(flag_val >> 16 == ENEMY_SLANDERER) {
+                where_flag_indicated_enemy_slanderer = getMapLocationFromFlagValue(flag_val);
+                round_when_flag_indicated_enemy_slanderer = rc.getRoundNum();
             }
-            if(index_in_places < 3) {
-                did_move = fuzzyStep(places[index_in_places]);
-            }
+        }
+        if(where_flag_indicated_enemy_slanderer != null
+            && 50 > rc.getRoundNum() - round_when_flag_indicated_enemy_slanderer
+        ) {
+            did_move = fuzzyStep(where_flag_indicated_enemy_slanderer);
         }
         return did_move;
     }
 
+    boolean flagEnemySlanderer() throws GameActionException {
+        // PRE: where_i_saw_enemy_slanderer and round_when_i_saw_enemy_slanderer
+        //   must be up-to-date for the current round.
+        boolean did_set_flag = false;
+        if(where_i_saw_enemy_slanderer != null
+            && rc.getRoundNum() - round_when_i_saw_enemy_slanderer < 3
+        ) {
+            int flag_val = getValueForFlagRelative(ENEMY_SLANDERER, where_i_saw_enemy_slanderer);
+            did_set_flag = trySetFlag(flag_val);
+            System.out.println("Tried to flag enemy slan at " + where_i_saw_enemy_slanderer.toString());
+        }
+        return did_set_flag;
+    }
+
 
     public void runTurnUnit() throws GameActionException {
-        mapEdgeFlagReceivingStuffNonEc();
-
-        flagEnemies();
-        flagMapEdges();
-        flagNeutralECs();
-        // The second flag overrides the first
-
         Team enemy = rc.getTeam().opponent();
         int sensorRadius = rc.getType().sensorRadiusSquared;
         MapLocation myLoc = rc.getLocation();
+
+        mapEdgeFlagReceivingStuffNonEc();
 
         /*
         Note, within the sensor radius, I can detect all attributes of robots.
@@ -88,9 +66,12 @@ public strictfp class Muckraker extends Unit {
             if (robot.type.canBeExposed()) {
                 // It's a slanderer... go get them!
                 if(
-                    round_when_i_saw_enemy_slanderer != rc.getRoundNum()
-                    || where_i_saw_enemy_slanderer == null
-                    || myLoc.distanceSquaredTo(robot.location) < myLoc.distanceSquaredTo(where_i_saw_enemy_slanderer)
+                    robot.influence >= 5 // Don't chase trick slanderers (but still expose them)
+                    && (
+                        round_when_i_saw_enemy_slanderer != rc.getRoundNum()
+                        || where_i_saw_enemy_slanderer == null
+                        || myLoc.distanceSquaredTo(robot.location) < myLoc.distanceSquaredTo(where_i_saw_enemy_slanderer)
+                    )
                 ) {
                     where_i_saw_enemy_slanderer = robot.location;
                     round_when_i_saw_enemy_slanderer = rc.getRoundNum();
@@ -102,6 +83,14 @@ public strictfp class Muckraker extends Unit {
                 }
             }
         }
+        // Now that where_i_saw_enemy_slanderer is accurate, we can flag the slanderer and more
+
+        flagEnemies();
+        flagMapEdges();
+        flagNeutralECs();
+        flagEnemySlanderer();
+        // The later flag overrides the earlier
+        
 
         RobotInfo nearest_enemy_pol = nearestRobot(null, -1, enemy, RobotType.POLITICIAN);
 
@@ -119,19 +108,21 @@ public strictfp class Muckraker extends Unit {
             // Try to move toward enemy slanderers seen recently
             if (
                 where_i_saw_enemy_slanderer != null
-                && 30 > rc.getRoundNum() - round_when_i_saw_enemy_slanderer
+                && 10 > rc.getRoundNum() - round_when_i_saw_enemy_slanderer
                 && fuzzyStep(where_i_saw_enemy_slanderer)
             ) {
                 System.out.println("I (muckraker) stepped toward target: " + where_i_saw_enemy_slanderer.toString());
             }
 
             if(is_cruncher) {
-                moveForSymmetricEnemyEc();
+                // This means if we are a guard,
+                //  then an enemy slan is reported,
+                //  then we stop being a guard,
+                //  we will not know about the enemy slan.
+                moveForSlandererFromFlag();
             }
 
-            if(!is_adj_to_enemy_ec) {
-                exploreMove();
-            }
+            exploreMove();
         }
     }
 }
